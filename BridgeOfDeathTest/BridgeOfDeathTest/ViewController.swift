4d5fd18a34a88d30ad22f0ee4c55c58a19b65e78
Angelo Di Paolo <angelod101@gmail.com>
2015-04-28 15:07:06 -0400

    add dropbox test URL in sample vc

--- BridgeOfDeathTest/BridgeOfDeathTest/ViewController.swift
+++ BridgeOfDeathTest/BridgeOfDeathTest/ViewController.swift
@@ -24,12 +24,10 @@ class ViewController: UIViewController {
     @IBAction func openWebView(sender: UIButton) {
         let webController = WebViewController.WithBridgePlatform()
         let navController = UINavigationController(rootViewController: webController)
-        let url = NSURL(string: "http://localhost:3000/")!
+        let url = NSURL(string: "https://dl.dropboxusercontent.com/u/6589453/wm/bridge/bridge-test.html")!
 //        let url = NSURL(string: "http://bridgeofdeath.herokuapp.com/")!
 
         webController.loadURL(url)
         presentViewController(navController, animated: true, completion: nil)
     }
-
 }
-
