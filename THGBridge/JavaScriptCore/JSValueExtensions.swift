4b5f731ded985f98dec28cba29887bec47bfa218
Angelo Di Paolo <angelod101@gmail.com>
2015-05-01 13:16:44 -0400

    support JS error in JSValue callback extension

--- THGBridge/JavaScriptCore/JSValueExtensions.swift
+++ THGBridge/JavaScriptCore/JSValueExtensions.swift
@@ -11,19 +11,34 @@ import JavaScriptCore
 internal extension JSValue {
     
     /**
-     Call the JavaScript function value with data and error params. The bridge 
-     API uses a popular convention for JavaScript callbacks: `function(error, data)`.
-     This method is convenience for invoking a callback that follows this convention.
+     Calls the value like it was a JavaScript function in the form of 
+     `function(error, data)`.
+     :param: data The data that is passed to the callback
+     :return: The return value of the function call.
     */
-    func callWithWithData(data: AnyObject, error: NSError?) -> JSValue! {
-        let args: [AnyObject]
-        
-        if let error = error {
-            args = [error, data]
-        } else {
-            args = [NSNull(), data]
-        }
-        
-        return callWithArguments(args)
+    func callWithData(data: AnyObject) -> JSValue! {
+        return callWithArguments([NSNull(), data])
+    }
+    
+    /**
+     Calls the value like it was a JavaScript function in the form of
+     `function(error, data)`.
+     :param: error The error that is passed to the callback.
+     :return: The return value of the function call.
+    */
+    func callWithError(error: NSError) -> JSValue! {
+        return callWithErrorMessage(error.localizedDescription)
+    }
+    
+    /**
+    Calls the value like it was a JavaScript function in the form of
+    `function(error, data)`.
+    :param: errorMessage The message used to create the JavaScript error
+     that is passed to the callback.
+    :return: The return value of the function call.
+    */
+    func callWithErrorMessage(errorMessage: String) -> JSValue! {
+        let jsError = JSValue(newErrorFromMessage: errorMessage, inContext: context)
+        return callWithArguments([jsError, NSNull()])
     }
 }
